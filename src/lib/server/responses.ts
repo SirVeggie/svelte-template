43cc5c0ca28fb910f2c2daa2163eba7468f28a0a
SirVeggie <32365239+SirVeggie@users.noreply.github.com>
2023-08-16 04:13:38 +0300

    add missing package

--- src/lib/server/responses.ts
+++ src/lib/server/responses.ts
@@ -2,7 +2,7 @@ import type { ServerError } from "$lib/types";
 
 export function error(message: string | ServerError, status = 500) {
     if (typeof message === 'string') message = { error: message };
-    return new Response(JSON.stringify({ error: message }), { status });
+    return new Response(JSON.stringify(message), { status });
 }
 
 export function success(message?: unknown, status = 200) {
